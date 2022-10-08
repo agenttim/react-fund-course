378f3b2e99fb95e601b3c0fb47f5b37d34ced124
agent_tim <agent_tim@mail.ru>
2022-10-08 18:06:23 +0500

    08.10.2022_1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
 import React, {useState} from "react";
+import Counter from "./component/Counter";
+import ClassCounter from "./component/ClassCounter";
+import "./styles/App.css";
+import PostItem from "./component/PostItem";
+import PostList from "./component/PostList";
+import MyButton from "./component/UI/button/MyButton";
 
 function App() {
-    const [likes, setLikes] = useState(5);
+    const [posts, setPosts] = useState([
+        {id: 1, title: "JavaScript", body: "Description"},
+        {id: 2, title: "JavaScript 2", body: "Description"},
+        {id: 3, title: "JavaScript 3", body: "Description"},
+    ])
 
-    function increment() {
-        setLikes(likes + 1)
-    }
 
-    function decrement() {
-        setLikes(likes - 1)
-    }
-
-  return (
-    <div className="App">
-        <h1>{likes}</h1>
-      <button onClick={increment}>Increment</button>
-      <button onClick={decrement}>Decrement</button>
-    </div>
-  );
+    return (
+        <div className="App">
+            <form>
+                <input type="text" placeholder="Название поста"/>
+                <input type="text" placeholder="Название поста"/>
+                <MyButton>Создать пост</MyButton>
+            </form>
+            <PostList posts={posts} title="Посты по JS"></PostList>
+        </div>
+    );
 }
 
 export default App;
